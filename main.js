1d8fecacfad12062841f561b44725d7975b75b7e
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-07-17 15:33:21 +0900

    make up app filtering

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const child_process=require("child_process")
+const { kMaxLength } = require("buffer");
+const child_process = require("child_process");
+const fs = require("fs");
+const { key } = require("localforage");
 
-const line_feed="\r\n"; //if you get any problems, this may be the reason.
+const line_feed = "\r\n"; //if you get any problems, this may be the reason.
 const data_templete = {
     name: "",
     ID: "",
@@ -8,63 +11,90 @@ const data_templete = {
     new_version: "",
     source: "",
 };
-const app_filter=JSON.parse(fs.readFileSync("./app_filter.json").toString());
+
+const app_filters_promise = read_json_async("./app_filters.json");
 
 console.log("executing 'winget upgrade'...");
-const winget_list=child_process.execFileSync("winget",["upgrade"]).toString().split(line_feed);
-const id_start = winget_list[0].indexOf("ID") - winget_list[0].indexOf("名前")+2;
-const all_list = winget_list
-    .slice(2, -3)
-    .map((element) =>
-        element
-            .split(" ")
-            .filter(filter_space)
-            .reverse()
-            .reduce(assemble_data, { ...data_templete })
-    );
+const winget_list = child_process
+    .execFileSync("winget", ["upgrade"])
+    .toString()
+    .split(line_feed);
+const id_start =
+    winget_list[0].indexOf("ID") - winget_list[0].indexOf("名前") + 2;
+const all_list = winget_list.slice(2, -3).map((element) =>
+    element
+        .split(" ")
+        .filter(filter_space)
+        .reverse()
+        .reduce(assemble_data, { ...data_templete })
+);
 
-const list=all_list.filter(filter_specified);
-const required_upgrade=list.length;
-let upgrading=0;
-for(let i=0;i<required_upgrade;i++){
-    //
+let app_filters;
+
+(async () => {
+    app_filters = await app_filters_promise;
+    console.log(app_filters);
+    const list = all_list.filter(filter_apps);
+    const required_upgrade = list.length;
+    let upgrading = 0;
+    for (let i = 0; i < required_upgrade; i++) {
+        //
+    }
+
+    fs.writeFileSync("./list.json", JSON.stringify(list));
+})();
+
+//functions
+
+async function read_json_async(path) {
+    return JSON.parse(await fs.promises.readFile(path, { encoding: "utf-8" }));
 }
 
-require("fs").writeFileSync("./list.json", JSON.stringify(list));
-
-function filter_space(element){
-    if(element===""){
+function filter_space(element) {
+    if (element === "") {
         return false;
-    }else{
+    } else {
         return true;
     }
 }
 
-function assemble_data(data,segment,index){
-    //console.log(data,segment,index);
-    switch(index){
+function assemble_data(app_data, segment, index) {
+    switch (index) {
         case 0:
-            data.source=segment;
+            app_data.source = segment;
             break;
 
         case 1:
-            data.new_version=segment;
+            app_data.new_version = segment;
             break;
 
         case 2:
-            data.previous_version=segment;
+            app_data.previous_version = segment;
             break;
 
         case 3:
-            data.ID=segment;
+            app_data.ID = segment;
             break;
 
         default:
-            data.name = `${segment} ${data.name}`.trimEnd();
+            app_data.name = `${segment} ${app_data.name}`.trimEnd();
     }
-    return data;
+    return app_data;
 }
 
-function filter_specified(app_data){
-    //
+function filter_apps(app_data) {
+    for (let i = 0; i < app_filters.length; i++) {
+        if (
+            Object.keys(app_filters[i]).every(
+                (key) => app_data[key] === app_filters[i][key]
+            )
+        ) {
+            return false;
+        }
+    }
+    return true;
 }
+
+function check_apps() {}
+
+function has_filter() {}
