de1f383df71002bdb30c2d36cea3af67380d2cd1
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2023-08-09 21:31:25 +0900

    undo before change about adopt path

--- winget-upgrade-custom.js
+++ winget-upgrade-custom.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cp_execFile = util.promisify(child_process.execFile);
 
 let app_filter_path;
-if (process.argv.length > 2 && !process.argv[2].startsWith("--")) {
+if (process.argv.length > 2) {
     app_filter_path = process.argv[2];
 } else {
     app_filter_path = "./app_filter.json";
